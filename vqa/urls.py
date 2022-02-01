3357e13a589ef18a6f3e536581e483240d18b4d3
Rasika Joshi <rasika2112@gmail.com>
2022-02-01 20:47:18 +0530

    Connected with firebase

--- vqa/urls.py
+++ vqa/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 #now import the views.py file into this code
 from . import views
 urlpatterns=[
-  path('',views.index)
+  path('',views.index, name="index")
 ]
--- vqa/views.py
+++ vqa/views.py
@@ -1,6 +1,34 @@
+from ast import Pass
+from pickle import GET
 from django.shortcuts import render
 from django.http import HttpResponse
+import pyrebase
+
+config = {
+        "apiKey": "AIzaSyDZ8DZQb7ulhRh6CZJQh4YIr_6U9cSi_aA",
+        "authDomain": "vqa11-fccb4.firebaseapp.com",
+        "databaseURL": "https://vqa11-fccb4-default-rtdb.firebaseio.com",
+        "projectId": "vqa11-fccb4",
+        "storageBucket": "vqa11-fccb4.appspot.com",
+        "messagingSenderId": "917773394703",
+        "appId": "1:917773394703:web:6605dba8619ed9def1547a"
+      }
+
+firebase = pyrebase.initialize_app(config)
+authe = firebase.auth()
+database = firebase.database()
 
 
 def index(request):
-    return render(request, 'index.html')
+    if request.method =="POST":
+        Users_data = database.child('Users').get().val()
+        Users = dict(Users_data.items())
+        # print(Users)
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        answer = request.POST.get("answer")
+        if username in Users.keys() and password == Users[username]:
+            print(answer) 
+        return render(request, 'index.html')
+    else:
+         return render(request, 'index.html')
